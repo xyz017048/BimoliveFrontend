cb0be39f5691d74848806280a8f3deec07bbd495
Xueyang Han <XueyangHan@Xueyangs-MacBook-Air.local>
2016-01-29 00:15:28 -0700

    student page.

--- app/scripts/controllers/studentCtrl/student.js
+++ app/scripts/controllers/studentCtrl/student.js
@@ -23,27 +23,24 @@ angular.module('bimoliveApp')
      * @return {[type]}           [description]
      */
     function getQuestions(sectionId) {
-        var result = [{ "content": "What is the broadcast showing, right now? I can't see anything. Is there any one can help me?", "username": "Doe" },
-            { "content": "What is the broadcast 2", "username": "Smith" },
-            { "content": "What is the broadcast showing,3", "username": "Jones" }];
-        /*
+        var result = [];
         $http( { 
             method: 'POST', 
-            url: 'http://bimolive.us-west-2.elasticbeanstalk.com/student/getQuestion',
-             headers: {
+            url: 'http://bimolive.us-west-2.elasticbeanstalk.com/getquestions',
+            headers: {
                     'Content-Type': undefined
             },
             data: {
-                idLecture: sectionId
+                roleLevel: 1,
+                idLecture: sectionId,
+                interval: 0
             }
         } )
         .success(function(data, status) {
-            if(data.result == 1) {
-                result = data.result;
-            } else {
-                console.log(data);
-                console.log(status);
-                console.log('Request failed');
+            console.log(data);
+            for(var d in data) {
+                // result.push({ "content": d.content, "username": d.username});
+                console.log(a);
             }
         })
         .error(function(data, status) {
@@ -51,10 +48,9 @@ angular.module('bimoliveApp')
             console.log(status);
             console.log('Request failed');
         });
-        */
         return result;
     }
-    
+
     /**
      * send question to server
      * @param  {[type]} idUser    [description]
@@ -62,21 +58,23 @@ angular.module('bimoliveApp')
      * @param  {[type]} sectionId [description]
      * @return {[type]}           [description]
      */
-    this.sendQuestion = function (idUser, username, sectionId) {
+    // function sendQuestion (idUser, username, sectionId) {
+    this.sendQuestion = function(sectionId) {
+        console.log('send ' + this.idUser 
+                + ' ' + this.username + ' ' 
+                 + sectionId + ' (' 
+                 + this.currentQuestion + ')');
+
         if (this.currentQuestion.trim() !== '') {
-            alert('send ' + idUser 
-                + ' ' + username + ' ' 
-                 + sectionId + ' ' 
-                 + this.currentQuestion);
             $http( { 
                 method: 'POST', 
                 url: 'http://bimolive.us-west-2.elasticbeanstalk.com/student/sendQuestion',
-                 headers: {
+                headers: {
                     'Content-Type': undefined
                 },
                 data: {
-                    idUser: idUser,
-                    username: username,
+                    idUser: this.idUser,
+                    username: this.username,
                     idLecture: sectionId,
                     content: this.currentQuestion.trim()
                 }
@@ -84,6 +82,12 @@ angular.module('bimoliveApp')
             .success(function(data, status) {
                 if(data.result) {
                     this.currentQuestion = '';
+                } else {
+                    alert('send ' + this.idUser 
+                    + ' ' + this.username + ' ' 
+                     + sectionId + ' ' 
+                     + this.currentQuestion
+                     + " " + data.result);
                 }
             })
             .error(function(data, status) {
@@ -91,18 +95,21 @@ angular.module('bimoliveApp')
                 console.log(status);
                 console.log('Request failed');
             });
-        } else {
-            alert(this.currentQuestion + 'it is empty after trim');
         }
     };
     
-    var videoId = $routeParams.id; //the current video id
+    var sectionId = $routeParams.id; //the current video id
+    this.idUser = '1';
+    this.username = 'xueyangh';
 
-    // call the function that connect the server and get 
-    // all video info and store in currentVideo
-    this.currentVideo = getCurrentVideo(videoId);
-    
     this.currentQuestion = '';
     
-    this.questions = getQuestions();
+    // call the function that connect the server and get 
+    // all video info and store in currentVideo
+    this.currentVideo = getCurrentVideo(sectionId);
+    
+    this.questions = getQuestions(sectionId);
+
+    // this.sendQuestion.value('idUser', '1').value('username', username);
+    // this.sendQuestion = this.sendQuestion(idUser, username, sectionId);
 });
