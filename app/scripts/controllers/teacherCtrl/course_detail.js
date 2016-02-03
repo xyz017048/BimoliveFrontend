3e9adb7809257aaaf9a84982f70b0792906d9035
XiangyuZhong <xiangyu0627@gmail.com>
2016-02-03 11:05:57 -0700

    course detail page

--- app/scripts/controllers/teacherCtrl/course_detail.js
+++ app/scripts/controllers/teacherCtrl/course_detail.js
@@ -9,6 +9,21 @@ angular.module('bimoliveApp')
     
     this.currentCourse = course.currentCourse;
     
+    
+    function getMycourses () {
+        var array = [];
+        for (var i = 0; i < 10; i++) {
+            var video = { "name": "Course #" + i,
+                "presenter": "Smith" + i,
+                "id": i
+            };
+            array.push(video);
+        }
+        return array;
+    }
+    
+    this.lectureList = getMycourses();
+    
     /**
      * 
      */
