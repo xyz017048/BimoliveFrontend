fb372d9f39af9290ca2094227653347b885ca851
XiangyuZhong <xiangyu0627@gmail.com>
2016-02-05 00:06:58 -0700

    merge

--- app/scripts/controllers/teacherCtrl/lecture_detail.js
+++ app/scripts/controllers/teacherCtrl/lecture_detail.js
@@ -7,17 +7,5 @@ angular.module('bimoliveApp')
  */
 .controller('LectureDetailCtrl', ['$http', 'course', function ($http, course) {
 
-    function getMycourses () {
-        var array = [];
-        for (var i = 0; i < 2; i++) {
-            var video = { "name": "Course #" + i,
-                "presenter": "Smith" + i,
-                "id": i
-            };
-            array.push(video);
-        }
-        return array;
-    }
-    
-    this.lectureList = getMycourses();
+    this.currentLecture = course.currentCourse;
 }]);
