0a95675c733682d5771be630691fab04308d6502
XiangyuZhong <xiangyu0627@gmail.com>
2016-02-08 17:37:33 -0700

    teacher lecture info

--- app/scripts/controllers/teacherCtrl/teacher.js
+++ app/scripts/controllers/teacherCtrl/teacher.js
@@ -110,4 +110,32 @@ angular.module('bimoliveApp')
         .error(function(data, status) {
         });
     };
+
+    this.currentLecture = {};
+    this.isFinished = false;
+   
+    if(MainService.getCurrentUser().roleLevel === 2)
+    {
+        $http( { 
+            method: 'POST', 
+            url: 'http://bimolive.us-west-2.elasticbeanstalk.com/teacher/singlelecture',
+            headers: {
+                'Content-Type': undefined
+            },
+            data: {
+                idLecture: $routeParams.idLecture,
+                idUser: MainService.getCurrentUser().idUser
+            }
+        } )
+        
+        .success(function(data, status) {
+            appScope.currentLecture = data;
+            if (data.status === 'replay') {
+                appScope.isFinished = true; 
+            }
+        })
+        
+        .error(function(data, status) {
+        });
+    }
 }]);
