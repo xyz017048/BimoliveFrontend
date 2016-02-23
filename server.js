16c050f3e77d27c64e5f13851a72d3fde4b32109
shengzihong <sam@sams-mbp-2.uconnect.utah.edu>
2016-02-23 15:02:38 -0700

    node server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+      app     = express();
+
+var port = process.env.PORT || 9000; 
+
+app.set('views', __dirname + '/app');
+app.set('view engine', 'jade');
+
+app.use(express.static('/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/scripts',  express.static(__dirname + '/app/scripts'));
+app.use('/styles',  express.static(__dirname + '/app/styles'));
+app.use('/views',  express.static(__dirname + '/app/views'));
+app.use('/images',  express.static(__dirname + '/app/images'));
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+app.listen(port);
+
+console.log('Server is running on port ' + port);
