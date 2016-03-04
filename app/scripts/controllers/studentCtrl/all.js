8710f96733d97c264ab16b73723f88afba8a423b
XiangyuZhong <xiangyu0627@gmail.com>
2016-03-04 01:59:43 -0700

    all live and replay views

--- app/scripts/controllers/studentCtrl/all.js
+++ app/scripts/controllers/studentCtrl/all.js
@@ -13,4 +13,46 @@ angular.module('bimoliveApp')
         $('#loginModal').modal('show'); 
     }
         
+    var appScope = this;
+    
+    this.liveVideos = [];
+    // getLiveLectures (videoInfo)
+    this.getLiveVideos = function () {
+        $http( { 
+            method: 'POST', 
+            url: 'http://bimolive.us-west-2.elasticbeanstalk.com/livevideos',
+            headers: {
+                'Content-Type': undefined
+            }
+        } )
+        .success(function(data, status) {
+            // put video to UI
+            appScope.liveVideos = data;
+        })
+        .error(function(data, status) {
+            console.log(data);
+            console.log(status);
+            console.log('Request failed');
+        });
+    };
+    
+    this.replayVideos = [];
+    this.getReplayVideos = function () {
+        $http( { 
+            method: 'POST', 
+            url: 'http://bimolive.us-west-2.elasticbeanstalk.com/replayvideos',
+            headers: {
+                'Content-Type': undefined
+            }
+        } )
+        .success(function(data, status) {
+            // put video to UI
+            appScope.replayVideos = data;
+        })
+        .error(function(data, status) {
+            console.log(data);
+            console.log(status);
+            console.log('Request failed');
+        });
+    };
 }]);
